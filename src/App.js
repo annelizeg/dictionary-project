dee9ead82a98cc8e5284a98a9573c5888c65104e
Annelize Grobbelaar <91416875+annelizeg@users.noreply.github.com>
2025-09-15 12:00:21 +0930

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header">
+        <h1>Hello Dictionary</h1>
+        <button className="btn btn-primary">Search</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
