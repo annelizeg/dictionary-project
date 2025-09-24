9cce189cf55459db7342c74314fdf45689b22b61
Annelize Grobbelaar <91416875+annelizeg@users.noreply.github.com>
2025-09-24 13:48:04 +0930

    Added Pexels attribute

--- src/Footer.js
+++ src/Footer.js
@@ -6,24 +6,36 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <span>Coded by </span> <span className="coder">Annelize Grobbelaar</span>{" "}
-      |
-      <a
-        href="https://www.linkedin.com/in/annelize-grobbelaar-1ba524151/"
-        title="LinkedIn"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LinkedIn
-      </a>{" "}
-      and is{" "}
-      <a
-        href="https://github.com/annelizeg/dictionary-project"
-        target="_blank"
-        rel="noreferrer"
-      >
-        open-sourced on GitHub
-      </a>
+      <div>
+        <span>Coded by </span>{" "}
+        <span className="coder">Annelize Grobbelaar</span> |
+        <a
+          href="https://www.linkedin.com/in/annelize-grobbelaar-1ba524151/"
+          title="LinkedIn"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/annelizeg/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </div>
+      <div>
+        Photos provided by{" "}
+        <a href="https://www.pexels.com" target="_blank" rel="noreferrer">
+          <img
+            src="https://images.pexels.com/lib/api/pexels-white.png"
+            alt=""
+            width="50em"
+          />
+        </a>
+      </div>
     </div>
   );
 }
