92b02fb95d8b37a39ed8d7747fa3861f35d5f73d
Annelize Grobbelaar <91416875+annelizeg@users.noreply.github.com>
2025-09-28 09:38:09 +0930

    Updated photoSearch function
    Only search for photos if a valid response is received from the dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,6 +15,7 @@ export default function Dictionary(props) {
     console.log(response);
     if (response.data.status !== "not_found") {
       setResults(response.data);
+      photoSearch();
     } else {
       alert(
         "Sorry, we couldn't find the word you were looking for. Please try again."
@@ -24,6 +25,15 @@ export default function Dictionary(props) {
 
   function handelPexelsResponse(response) {
     setPhotos(response.data.photos);
+  }
+
+  function photoSearch() {
+    //Documentation: Pexels Images/Videos API - https://www.pexels.com/api/documentation?language=javascript#introduction
+    const pexelsApiKey =
+      "7LiLP7QOB5wMYTTiXOfk5S2LFFke2cLauEI2gYEGr9ofmhFQZGXj1OSo";
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    const headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers }).then(handelPexelsResponse);
   }
 
   function search() {
@@ -38,13 +48,6 @@ export default function Dictionary(props) {
         console.log(error);
         alert("Sorry, an unexpected error occured. Please try again.");
       });
-
-    //Documentation: Pexels Images/Videos API - https://www.pexels.com/api/documentation?language=javascript#introduction
-    const pexelsApiKey =
-      "7LiLP7QOB5wMYTTiXOfk5S2LFFke2cLauEI2gYEGr9ofmhFQZGXj1OSo";
-    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    const headers = { Authorization: `${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers }).then(handelPexelsResponse);
   }
 
   function load() {
@@ -72,9 +75,10 @@ export default function Dictionary(props) {
                 <input
                   type="search"
                   placeholder="Search for a word"
-                  autoFocus
+                  autoFocus="on"
                   onChange={handelKeywordChange}
-                  aria-describedby="passwordHelpInline"
+                  value={keyword}
+                  onFocus={() => setKeyword("")} // clears on focus
                 />
                 <div className=" hint">
                   Suggested words: sunset, wine, forest, animals...
